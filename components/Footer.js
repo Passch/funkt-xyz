eeea52e15b4bd9a1c9029340067d3195056bff86
Passch <pc@passch.com>
2022-11-23 02:16:11 +0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        design@funkt.xyz | space@funkt.xyz
+        funkt.xyz
       </footer>
     </>
   )
