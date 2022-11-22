c912b43232539ccd9eec4566b3096a0299cc7774
Passch <pc@passch.com>
2022-11-23 01:58:58 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Hello, it's funkt()" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+        Full website coming soon!
         </p>
       </main>
 
